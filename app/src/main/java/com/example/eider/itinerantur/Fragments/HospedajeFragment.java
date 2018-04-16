c10682265aa6b02a7986d1bb20ca149f59c14238
eider <eiderdiazmoraila@hotmail.com>
2018-04-16 14:39:31 -0700

    [add] fragmento de Hospedaje vacio

---
+++ app/src/main/java/com/example/eider/itinerantur/Fragments/HospedajeFragment.java
@@ -0,0 +1,37 @@
+package com.example.eider.itinerantur.Fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.eider.itinerantur.R;
+
+
+public class HospedajeFragment extends Fragment {
+
+    public HospedajeFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+        }
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_comida, container, false);
+
+    }
+
+
+
+
+}
