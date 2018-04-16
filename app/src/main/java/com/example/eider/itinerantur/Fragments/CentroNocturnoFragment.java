394fe8ebdb372f13edf80ab87a6bf377c9d9faba
Itineratum <itineratum2018@gmail.com>
2018-04-16 15:38:10 -0600

    Fragmentos Daniel Hernandez centros nocturnos Promociones Tursimo Alternativo Actividades Municipales

---
+++ app/src/main/java/com/example/eider/itinerantur/Fragments/CentroNocturnoFragment.java
@@ -0,0 +1,38 @@
+package com.example.eider.itinerantur.Fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.eider.itinerantur.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class CentroNocturnoFragment extends Fragment {
+
+
+    public CentroNocturnoFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+        }
+
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_centro_nocturno, container, false);
+    }
+
+}
